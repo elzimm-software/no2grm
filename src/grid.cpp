22801b977a9ee797c3af55c4624ba8b4a65616ea
Elijah Zimmerman <elijahz0409@gmail.com>
2026-03-03 13:35:42 -0500

    Refactor grid render logic into Grid

--- src/grid.cpp
+++ src/grid.cpp
@@ -1,7 +1,27 @@
 #include "grid.h"
 
-grid::Grid::Grid(const unsigned int n_rows, const unsigned int n_cols): n_rows(n_rows), n_cols(n_cols) {
-    arr = new CellType[n_rows * n_cols] {EMPTY};
+#include "sdl_config.h"
+#include "style.h"
+
+namespace {
+    unsigned int compute_point_array_size(const unsigned int n_cells) {
+        return (n_cells + 1) * 4;
+    }
+
+    unsigned int compute_offset(const unsigned int max_len, const unsigned int min_len) {
+        if (max_len < min_len) {
+            return 0;
+        }
+
+        return (max_len - min_len) / 2;
+    }
+}
+
+grid::Grid::Grid(const unsigned int n_rows, const unsigned int n_cols) : vertical(n_cols, WINDOW_HEIGHT),
+                                                                         horizontal(n_rows, WINDOW_WIDTH) {
+    arr = new CellType[vertical.n_cells * horizontal.n_cells]{EMPTY};
+    n_elements = (vertical.n_elements + horizontal.n_elements) / 2;
+    line_points = new unsigned int[n_elements];
 }
 
 grid::CellType grid::Grid::get_cell(const unsigned int x, const unsigned int y) const {
@@ -9,17 +29,41 @@ grid::CellType grid::Grid::get_cell(const unsigned int x, const unsigned int y) 
 }
 
 void grid::Grid::set_cell(const CellType cell_type, const unsigned x, const unsigned y) const {
-    *(arr + get_index(x,y)) = cell_type;
+    *(arr + get_index(x, y)) = cell_type;
 }
 
 unsigned int grid::Grid::get_n_cols() const {
-    return n_cols;
+    return horizontal.n_cells;
 }
 
 unsigned int grid::Grid::get_n_rows() const {
-    return n_rows;
+    return vertical.n_cells;
+}
+
+void grid::Grid::draw_grid(SDL_Renderer* renderer) {
+}
+
+void grid::Grid::compute_grid_points() const {
+    compute_line_points(vertical, horizontal, 0);
+    compute_line_points(horizontal, vertical, vertical.n_elements);
+}
+
+grid::Grid::RenderGridInfo::RenderGridInfo(unsigned int n_cells, unsigned int max_size) : n_cells(n_cells),
+    n_elements(compute_point_array_size(n_cells)), grid_size(n_cells * CELL_SIZE) {
+    offset = compute_offset(max_size, grid_size);
+}
+
+void grid::Grid::compute_line_points(const RenderGridInfo& parallel, const RenderGridInfo& perpendicular,
+                                     const unsigned int offset) const {
+    for (unsigned int i = offset, cell_pos = perpendicular.offset; i < parallel.n_cells + 1;
+         i++, cell_pos += CELL_SIZE) {
+        *(line_points + i) = cell_pos;
+        *(line_points + i + 1) = parallel.offset;
+        *(line_points + i + 2) = cell_pos;
+        *(line_points + i + 3) = parallel.offset + parallel.grid_size;
+    }
 }
 
 unsigned int grid::Grid::get_index(const unsigned int x, const unsigned int y) const {
-    return x + n_cols * y;
+    return x + get_n_cols() * y;
 }
--- src/include/grid.h
+++ src/include/grid.h
@@ -1,8 +1,8 @@
 #ifndef NO2GRM_GRID_H
 #define NO2GRM_GRID_H
+#include <SDL3/SDL_render.h>
 
 namespace grid {
-
     enum CellType {
         FILLED,
         MARKED,
@@ -21,12 +21,30 @@ namespace grid {
 
         unsigned int get_n_rows() const;
 
+        void draw_grid(SDL_Renderer* renderer);
+
+        void compute_grid_points() const;
+
     private:
-        unsigned int n_rows;
-        unsigned int n_cols;
+        struct RenderGridInfo {
+            unsigned int n_cells;
+            unsigned int n_elements;
+            unsigned int grid_size;
+            unsigned int offset;
+
+            RenderGridInfo(unsigned int n_cells, unsigned int max_size);
+        };
+
         CellType* arr;
+        unsigned int* line_points;
+        unsigned int n_elements;
+        RenderGridInfo vertical;
+        RenderGridInfo horizontal;
 
         unsigned int get_index(unsigned int x, unsigned int y) const;
+
+        void compute_line_points(const RenderGridInfo& parallel, const RenderGridInfo& perpendicular,
+                                 unsigned int offset) const;
     };
 } // namespace grid
 
